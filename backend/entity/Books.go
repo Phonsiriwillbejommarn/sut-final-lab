8da6f9e376d11469e544ee7b1b7274404d0b71a2
Phonsiriwillbejommarn <thabunsri42@gmail.com>
2026-01-07 12:11:45 +0700

    เเก้ไขตัวหนังสือ

--- backend/entity/Books.go
+++ backend/entity/Books.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Books struct {
 	gorm.Model
-	Title string  `valid:"length(3|100)~Title must long 3-100"`
-	Price float64 `valid:"rage(50|5000)~“Price must be between 50 and 5000"`
+	Title string  `valid:"length(3|100)~Title must long more 3-100"`
+	Price float64 `valid:"rage(50|5000)~Price must be between 50 and 5000"`
 	Code  string  `valid:"matches([B][k]\\d{6}$)~Code must start with BK 
 	followed by 6 digits (0-9)"`
 }
