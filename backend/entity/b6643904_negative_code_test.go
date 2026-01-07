0f0d54c17dcb2760d797d1cb7e13558337dbc764
Phonsiriwillbejommarn <thabunsri42@gmail.com>
2026-01-07 12:05:50 +0700

    Closes #4

---
+++ backend/entity/b6643904_negative_code_test.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"github.com/asaskevich/govalidtor"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestnegativePrice(t *testing.T) {
+	g := NewGomegaWithT(t)
+	books := Books{
+		Title: "Jommarn",
+		Price: 55,
+		Code:  "BK1234568785",
+	}
+
+	ok, err := govalidtor.Struct(books)
+	g.Expect(ok).To(BeFalse())
+	g.Expect(err).ToNot(BeNil())
+	g.Expect(err.Error().ContainSubString("Code"))
+
+}
