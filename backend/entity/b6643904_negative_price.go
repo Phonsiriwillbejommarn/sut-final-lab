197cd0981de9192355fa0e48970efe10792abfa9
Phonsiriwillbejommarn <thabunsri42@gmail.com>
2026-01-07 11:55:51 +0700

    Closes #3

--- backend/entity/b6643904_negative_price.go
+++ backend/entity/b6643904_negative_price.go
@@ -1,21 +1,23 @@
 package entity
 
 import (
+	"testing"
+
 	"github.com/asaskevich/govalidtor"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
-func TestPositive(t *testing.T) {
+func TestnegativePrice(t *testing.T) {
 	g := NewGomegaWithT(t)
 	books := Books{
 		Title: "Jommarn",
-		Price: 51,
+		Price: 0,
 		Code:  "BK123456",
 	}
 
 	ok, err := govalidtor.Struct(books)
-	g.Expect(ok).To(BeTrue())
-	g.Expect(err).To(BeNil())
+	g.Expect(ok).To(BeFalse())
+	g.Expect(err).ToNot(BeNil())
+	g.Expect(err.Error().ContainSubString())
 
 }
